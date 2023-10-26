ac9aebab79654a8aca2eb6a5d848b6396bb16c49
Andrew Solomon <andrewsolomon610@gmail.com>
2023-10-26 07:27:37 -0700

    Adjusted graphics to have items properly placed

--- graphic.py
+++ graphic.py
@@ -21,6 +21,142 @@ class FFGraphic:
     PLAYER_URL = "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/{player_id}.png&w={player_width}&h={player_height}&cb=1"
     TEAM_DEFENSE_URL = "https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/{team_name}.png&w={team_width}&h={team_height}&cb=1"
 
+    def add_player(self, player_img, player_name, team_color_scheme):
+        padding = 15
+        # Add the player name
+        player_font = ImageFont.truetype(self.FONT_URL, self.PLAYER_NAME_FONT_SIZE)
+        txt_width, _ = player_font.getsize(player_name)
+        if txt_width > PLAYER_ICON_WIDTH:
+            # we place the text in its spot and move the icon
+            y = 452
+            x = self.WIDTH - txt_width - padding
+            self.drawer.text(
+                (x + 1.5, y + 2),
+                player_name,
+                team_color_scheme.secondary,
+                font=player_font,
+            )
+            self.drawer.text(
+                (x, y), player_name, team_color_scheme.tertiary, font=player_font
+            )
+            txt_midpoint = x + txt_width // 2
+            img_x = (txt_midpoint - PLAYER_ICON_WIDTH // 2) - padding
+
+            # Add the player image
+            img_height = self.HEIGHT - PLAYER_ICON_HEIGHT
+
+            self.img.paste(
+                player_img,
+                (
+                    img_x,
+                    img_height - 60,
+                ),
+                player_img,
+            )
+            pass
+        else:
+            # we place the icon in its spot and move the text
+
+            # Add the player image
+            img_width = self.WIDTH - PLAYER_ICON_WIDTH - 15
+            img_height = self.HEIGHT - PLAYER_ICON_HEIGHT
+
+            self.img.paste(
+                player_img,
+                (
+                    img_width,
+                    img_height - 60,
+                ),
+                player_img,
+            )
+
+            img_midpoint = img_width + PLAYER_ICON_WIDTH / 2
+
+            y = 452
+            x = img_midpoint - txt_width / 2
+
+            self.drawer.text(
+                (x + 1.5, y + 2),
+                player_name,
+                team_color_scheme.secondary,
+                font=player_font,
+            )
+            self.drawer.text(
+                (x, y), player_name, team_color_scheme.tertiary, font=player_font
+            )
+
+    def add_team_icon(self, team_img, team_name, team_color_scheme):
+        team_font = ImageFont.truetype(self.FONT_URL, self.TEAM_NAME_FONT_SIZE)
+        txt_width, _ = team_font.getsize(team_name)
+
+        txt_x, txt_y = (8, 80)
+
+        img_x, img_y = (26, 8)
+
+        # Add the LM name (team name)
+        self.drawer.text(
+            (txt_x + 1, txt_y + 1),
+            team_name,
+            team_color_scheme.secondary,
+            font=team_font,
+        )
+        self.drawer.text(
+            (txt_x, txt_y), team_name, team_color_scheme.tertiary, font=team_font
+        )
+
+        # Add the LM image (team profile pic)
+        bigsize = (team_img.size[0] * 5, team_img.size[1] * 5)
+        mask = Image.new("L", bigsize, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0) + bigsize, fill=255)
+        mask = mask.resize(team_img.size, Image.ANTIALIAS)
+        team_img.putalpha(mask)
+
+        txt_midpoint = txt_x + txt_width // 2
+        img_x = txt_midpoint - team_img.size[0] // 2
+        self.img.paste(team_img, (img_x, img_y), team_img)
+
+    def add_achievement(self, achievement_title, result, unit, team_color_scheme):
+        # Add the achievement title
+        achievement_font = ImageFont.truetype(self.FONT_URL, self.ACHIEVEMENT_FONT_SIZE)
+        x, y = 26, 165
+        achvmt_width, achvmt_height = achievement_font.getsize(achievement_title)
+
+        if achvmt_width + x > self.WIDTH:
+            achievement_title = achievement_title.replace(" ", "\n")
+            y = y - achvmt_height
+
+        self.drawer.text(
+            (x + 3, y + 3),
+            achievement_title,
+            team_color_scheme.secondary,
+            font=achievement_font,
+        )
+        self.drawer.text(
+            (x, y), achievement_title, team_color_scheme.tertiary, font=achievement_font
+        )
+
+        # Add the achievement result
+        x, y = 27, 227
+        if unit == "%":
+            result = round((result[0] / result[1]) * 100)
+        else:
+            result = result[0]
+        achievement_result = f"{result} {unit.upper()}"
+        self.drawer.text(
+            (x + 3, y + 3),
+            achievement_result,
+            team_color_scheme.secondary,
+            font=achievement_font,
+        )
+        self.drawer.text(
+            (x, y),
+            achievement_result,
+            team_color_scheme.tertiary,
+            font=achievement_font,
+        )
+        pass
+
     def __init__(
         self,
         team_data: dict,
@@ -60,83 +196,30 @@ class FFGraphic:
             team_img = Image.open(BytesIO(team_img))
         else:
             # For regular PNGS
-            r = requests.get(team_url, stream=True)
+            headers = {
+                "User-Agent": "FantasyFootballBot/0.0 (andrewsolomon610@gmail.com)"
+            }
+            r = requests.get(team_url, stream=True, headers=headers)
+            print(team_url)
             team_img = Image.open(r.raw)
 
         team_img = team_img.resize((TEAM_LOGO_WIDTH, TEAM_LOGO_HEIGHT), Image.ANTIALIAS)
 
         team_color_scheme: TeamColor = player_data["proTeamColorScheme"]
 
-        img = Image.new("RGB", (self.WIDTH, self.HEIGHT))
-        drawer = ImageDraw.Draw(img)
+        self.img = Image.new("RGB", (self.WIDTH, self.HEIGHT))
+        self.drawer = ImageDraw.Draw(self.img)
 
-        img.paste(
+        # Add the background color
+        self.img.paste(
             team_color_scheme.primary,
             (0, 0, self.WIDTH, self.HEIGHT),
         )
 
-        img.paste(
-            player_img,
-            (
-                self.WIDTH - PLAYER_ICON_WIDTH - 15,
-                self.HEIGHT - PLAYER_ICON_HEIGHT - 60,
-            ),
-            player_img,
-        )
+        self.add_player(player_img, player_name, team_color_scheme)
 
-        bigsize = (team_img.size[0] * 5, team_img.size[1] * 5)
-        mask = Image.new("L", bigsize, 0)
-        draw = ImageDraw.Draw(mask)
-        draw.ellipse((0, 0) + bigsize, fill=255)
-        mask = mask.resize(team_img.size, Image.ANTIALIAS)
-        team_img.putalpha(mask)
+        self.add_team_icon(team_img, team_name, team_color_scheme)
 
-        img.paste(team_img, (26, 8), team_img)
+        self.add_achievement(achievement_title, result, unit, team_color_scheme)
 
-        team_font = ImageFont.truetype(self.FONT_URL, self.TEAM_NAME_FONT_SIZE)
-        drawer.text(
-            (8 + 1, 80 + 1), team_name, team_color_scheme.secondary, font=team_font
-        )
-        drawer.text((8, 80), team_name, team_color_scheme.tertiary, font=team_font)
-
-        player_font = ImageFont.truetype(self.FONT_URL, self.PLAYER_NAME_FONT_SIZE)
-        x, y = 194, 452
-        drawer.text(
-            (x + 1.5, y + 2),
-            player_name,
-            team_color_scheme.secondary,
-            font=player_font,
-        )
-        drawer.text((x, y), player_name, team_color_scheme.tertiary, font=player_font)
-
-        achievement_font = ImageFont.truetype(self.FONT_URL, self.ACHIEVEMENT_FONT_SIZE)
-        x, y = 26, 165
-        drawer.text(
-            (x + 3, y + 3),
-            achievement_title,
-            team_color_scheme.secondary,
-            font=achievement_font,
-        )
-        drawer.text(
-            (x, y), achievement_title, team_color_scheme.tertiary, font=achievement_font
-        )
-
-        x, y = 27, 227
-        if unit == "%":
-            result = round((result[0] / result[1]) * 100)
-        else:
-            result = result[0]
-        achievement_result = f"{result} {unit.upper()}"
-        drawer.text(
-            (x + 3, y + 3),
-            achievement_result,
-            team_color_scheme.secondary,
-            font=achievement_font,
-        )
-        drawer.text(
-            (x, y),
-            achievement_result,
-            team_color_scheme.tertiary,
-            font=achievement_font,
-        )
-        img.save("image.png")
+        self.img.save("image.png")
